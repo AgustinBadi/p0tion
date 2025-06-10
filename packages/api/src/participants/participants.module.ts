b27bfec6e903afec1ef48f0717bb4e2155279a3a
jose tomas ruiz <j_tomas_93@hotmail.com>
2025-06-10 17:14:18 -0400

    fixed import of test files

--- packages/api/src/participants/participants.module.ts
+++ packages/api/src/participants/participants.module.ts
@@ -1,13 +1,18 @@
 import { Module } from "@nestjs/common"
 import { ParticipantsController } from "./controller/participants.controller"
 import { ParticipantsService } from "./service/participants.service"
-import { CeremoniesService } from "src/ceremonies/service/ceremonies.service"
+//import { CeremoniesService } from "src/ceremonies/service/ceremonies.service"
+import { CeremoniesService } from "../ceremonies/service/ceremonies.service"
 import { SequelizeModule } from "@nestjs/sequelize"
-import { CeremonyEntity } from "src/ceremonies/entities/ceremony.entity"
-import { CircuitsService } from "src/circuits/service/circuits.service"
-import { CircuitEntity } from "src/circuits/entities/circuit.entity"
+//import { CeremonyEntity } from "src/ceremonies/entities/ceremony.entity"
+import { CeremonyEntity } from "../ceremonies/entities/ceremony.entity"
+//import { CircuitsService } from "src/circuits/service/circuits.service"
+import { CircuitsService } from "../circuits/service/circuits.service"
+//import { CircuitEntity } from "src/circuits/entities/circuit.entity"
+import { CircuitEntity } from "../circuits/entities/circuit.entity"
 import { ParticipantEntity } from "./entities/participant.entity"
-import { ContributionEntity } from "src/circuits/entities/contribution.entity"
+//import { ContributionEntity } from "src/circuits/entities/contribution.entity"
+import { ContributionEntity } from "../circuits/entities/contribution.entity"
 
 @Module({
     controllers: [ParticipantsController],
