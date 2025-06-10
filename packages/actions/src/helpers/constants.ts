5cca1f59673e00c43256bab89cc2fd8665ea05c2
jose tomas ruiz <j_tomas_93@hotmail.com>
2025-06-10 17:17:06 -0400

    modify constant

--- packages/actions/src/helpers/constants.ts
+++ packages/actions/src/helpers/constants.ts
@@ -1,7 +1,7 @@
 // Main part for the Hermez Phase 1 Trusted Setup URLs to download PoT files.
-export const potFileDownloadMainUrl = `https://hermez.s3-eu-west-1.amazonaws.com/`
+export const potFileDownloadMainUrl = `https://pse-trusted-setup-ppot.s3.eu-central-1.amazonaws.com/pot28_0080/`
 // Main part for the Hermez Phase 1 Trusted Setup PoT files to be downloaded.
-export const potFilenameTemplate = `powersOfTau28_hez_final_`
+export const potFilenameTemplate = `ppot_0080_`
 // The genesis zKey index.
 export const genesisZkeyIndex = `00000`
 // The number of exponential iterations to be executed by SnarkJS when finalizing the ceremony.
